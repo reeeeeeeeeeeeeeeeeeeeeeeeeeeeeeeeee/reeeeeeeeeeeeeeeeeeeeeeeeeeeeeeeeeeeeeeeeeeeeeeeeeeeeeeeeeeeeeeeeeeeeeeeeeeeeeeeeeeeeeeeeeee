8ad39ccb528855b816e8f0d9ba2efb508ae4b681
Scott Mansfield <sgmansf@gmail.com>
2018-12-10 20:33:54 -0800

    Adding C# implementation

---
+++ reee.cs
@@ -0,0 +1,12 @@
+using System;
+public class reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+{
+    public static void Main() 
+    {
+        Console.Write('r');
+        while (true)
+        {
+            Console.Write('e');
+        }
+    }
+}
