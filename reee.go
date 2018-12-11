8d4c91e311b88e4da2ce95d316c131837547b95a
Jaden Weiss <jadr2ddude@gmail.com>
2018-12-10 21:11:23 -0500

    HTTP/REEEEEEEEEEEE

--- reee.go
+++ reee.go
@@ -1,8 +1,26 @@
 package main
 
+import "net/http"
+
 func main() {
-	print("r")
-	for {
-		print("e")
-	}
+	go func() {
+		print("r")
+		for {
+			print("e")
+		}
+	}()
+	panic( //REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+		http.ListenAndServe(":6552", // related joke
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusTeapot)
+				w.Write([]byte{'R'})
+				buf := make([]byte, 1024)
+				for i := range buf {
+					buf[i] = 'E'
+				}
+				for i := 0; i < 65*52*4; i++ {
+					w.Write(buf)
+				}
+			})),
+	)
 }
