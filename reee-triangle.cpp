fc71697453090f2d2e46afc3ac9dba54ba7b0a83
Alex Marginean <alexmarginean16@gmail.com>
2019-01-08 18:19:52 +0200

    Creats a triangle or reeee in c++

---
+++ reee-triangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int ee = 1
+	cout << "r" << endl;
+	while(true)
+	{
+		ee++;
+		for(int e = 1;e <= ee;e++)
+			cout << "e" << " ";
+		cout << endl;
+
+	}
+}
