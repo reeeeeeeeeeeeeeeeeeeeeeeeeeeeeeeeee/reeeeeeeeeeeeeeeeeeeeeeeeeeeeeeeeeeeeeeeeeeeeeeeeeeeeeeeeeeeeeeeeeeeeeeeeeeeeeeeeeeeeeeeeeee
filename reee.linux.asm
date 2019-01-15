bd7b403c636c7298b554d7a25ab9f602e9a928a8
Sires <ranearodrigo1@gmail.com>
2019-01-15 21:51:49 -0200

    Sorry didn't test before making the PR >.<
    (I thought it would be that easy)

--- reee.linux.asm
+++ reee.linux.asm
@@ -2,6 +2,10 @@
 ;  nasm -f elf reee.asm
 ;  ld -s -o ree ree.o
 
+section .data
+r db 'r'
+e db 'e'
+
 section .text
 global _start
 
@@ -9,16 +13,13 @@ _start:
 
 ;write to stdout
 mov edx,1   ;msg len
-mov ecx,'r' ;letter 'r'
+mov ecx,r   ;letter 'r'
 mov ebx,1   ;file desc
 mov eax,4   ;sys call num
 int 0x80    ;call kernel
 
+mov ecx,e   ;letter 'e'
 eeee:
-;write to stdout
-mov edx,1   ;msg len
-mov ecx,'e' ;letter 'e'
-mov ebx,1   ;file desc
-mov eax,4   ;sys call num
+mov eax,4
 int 0x80    ;call kernel
 jmp eeee    ;infinite loop!
