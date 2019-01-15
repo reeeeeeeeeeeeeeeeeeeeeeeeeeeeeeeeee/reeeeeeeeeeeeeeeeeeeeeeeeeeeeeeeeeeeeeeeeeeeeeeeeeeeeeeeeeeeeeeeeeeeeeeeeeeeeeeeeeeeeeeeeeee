757b77970bfae879b12803b87fa044f427cf15f5
Sires <ranearodrigo1@gmail.com>
2019-01-15 21:16:01 -0200

    eeeeeeeeeEEEeeEEeeEEeeeEEeeeEEeeeEeeeEE in linuks asm
    eEEeeeeEeeeeeEeeeEEeeeEEeeeEEeeeEeeeEEeee
    
    (Seriously speaking, now the assembly one will output infinite Es instead of just 'reee')

--- reee.linux.asm
+++ reee.linux.asm
@@ -5,22 +5,20 @@
 section .text
 global _start
 
-section .data
-msg db  'reee',0xa
-len equ $ - msg
-
-section .text
-;entry point
 _start:
 
 ;write to stdout
-mov edx,len ;msg len
-mov ecx,msg ;msg to write
+mov edx,1   ;msg len
+mov ecx,'r' ;letter 'r'
 mov ebx,1   ;file desc
 mov eax,4   ;sys call num
 int 0x80    ;call kernel
 
-;exit with kernel
-mov ebx,0   ;proc exit
-mov eax,1   ;sys call num
-int 0x80    ;call kernel, no return
+eeee:
+;write to stdout
+mov edx,1   ;msg len
+mov ecx,'e' ;letter 'e'
+mov ebx,1   ;file desc
+mov eax,4   ;sys call num
+int 0x80    ;call kernel
+jmp eeee    ;infinite loop!
