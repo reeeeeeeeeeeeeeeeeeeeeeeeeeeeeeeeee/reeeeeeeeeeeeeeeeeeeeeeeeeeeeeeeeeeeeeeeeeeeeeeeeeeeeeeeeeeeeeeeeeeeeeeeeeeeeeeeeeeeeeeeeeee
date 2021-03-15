2890dc613739de041f983b0c44ed94b6505a4d18
toydotgame <64190512+toydotgame@users.noreply.github.com>
2021-03-15 18:38:18 +1000

    Update reee.java

--- reee.java
+++ reee.java
@@ -1,8 +1,8 @@
 public class reee {
 	public static void main(String[] args) {
-		System.out.print('r');
-		for (;;) {
-			System.out.print('e');
+		System.out.print("r");
+		while(true) {
+			System.out.print("e");
 		}
 	}
 }
